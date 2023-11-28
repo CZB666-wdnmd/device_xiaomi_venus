ab6a770631be2cd181bf4dd3883b4e61a6f56d32
Joey Huab <joey@evolution-x.org>
2023-11-28 15:48:08 +0900

    venus: Refactor tree
    * Checkout to https://github.com/crdroidandroid/android_device_xiaomi_venus
    
    Co-authored-by: FlowerSea0208 <zzp060208@gmail.com>

--- evolution_venus.mk
+++ evolution_venus.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -17,7 +17,9 @@ $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 EVO_BUILD_TYPE := OFFICIAL
 TARGET_SUPPORTS_QUICK_TAP := true
 EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BUILD_APERTURE_CAMERA := false
 
+# Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := venus
 PRODUCT_MANUFACTURER := Xiaomi
@@ -25,5 +27,15 @@ PRODUCT_MODEL := M2011K2G
 PRODUCT_NAME := evolution_venus
 
 TARGET_BOOT_ANIMATION_RES := 1440
+
+PRODUCT_SYSTEM_NAME := venus_global
+PRODUCT_SYSTEM_DEVICE := venus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="venus_global-user 13 RKQ1.211001.001 V14.0.7.0.TKBMIXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := Xiaomi/venus_global/venus:13/RKQ1.211001.001/V14.0.7.0.TKBMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
